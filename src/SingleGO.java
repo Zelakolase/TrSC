d25e0a407ef4743ff892b80b19c5cd147311ac1f
Morad <460583232@sharkia1.moe.edu.eg>
2023-12-10 11:11:22 +0200

    Simplify; More Intelligent output

--- src/SingleGO.java
+++ src/SingleGO.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.Set;
 
 import algorithms.Clustering;
+import algorithms.GPI;
 import lib.GORead;
 import lib.Graph;
 
@@ -17,51 +18,33 @@ import lib.Graph;
 public class SingleGO {
     public static void main(String[] args) {
         long AllF = System.nanoTime();
-        double headCutoff = 0.35;
-        double transitionCutoff = 0.15;
-        double selectPercent = 0.75;
+        double headCutoff = 0.4;
+        double transitionCutoff = 0.1;
+        double selectPercent = 0.95;
 
-        Graph G = new GORead("../data/GO:0000003/interactions.tsv", "node1", "node2", "combined_score").G;
+        Graph G = new GORead("../data/GO:0042592/interactions.tsv", "node1", "node2", "combined_score").G;
         //Graph G = new GORead("../data/ALS.tsv", "node1", "node2", "combined_score").G;
         Clustering obj = new Clustering(G);
         long TimeF = System.nanoTime();
-        ArrayList<Graph> results = obj.cluster(headCutoff, transitionCutoff, 1.0, selectPercent);
+        ArrayList<Graph> results = obj.cluster(headCutoff, transitionCutoff, selectPercent);
         TimeF = System.nanoTime() - TimeF;
 
-        double meanClusterSize = 0;
-        double n = 0; // Number of clusters
-        ArrayList<Double> GCRs = new ArrayList<>(); // Value for each cluster
-        HashMap<Double, Set<String>> table = new HashMap<>();
-        double nOverHalf = 0; // Number of clusters over or equal GCR:0.5
+        /* This table has ArrayList of GCR and RC, then a Set of node names for each cluster */
+        HashMap<ArrayList<Double>, Set<String>> table = new HashMap<>();
 
         for(Graph sub : results) {
-            double GCR = algorithms.ClusterStats.GCR(sub);
-            if(sub.G.keySet().size() > 1) {
-                table.put(GCR, sub.G.keySet());
-                GCRs.add(GCR);
-                meanClusterSize += sub.G.size();
-                n++;
-            }
-            
-            if(GCR >= 0.5) nOverHalf ++;
+            double GCR = algorithms.Stats.GCR(sub);
+            double RC = algorithms.Stats.RC(sub);
+            if(sub.G.keySet().size() > 2) table.put(new ArrayList<>() {{add(RC); add(GCR);}}, sub.G.keySet());
         }
-        meanClusterSize /= n;
 
-        double sum = 0.0;
-        for (double num : GCRs) sum += num;
-        double mean = sum / GCRs.size();
-
-        double variance = 0.0;
-        for (double num : GCRs) variance += Math.pow(num - mean, 2);
-        double stddev = Math.sqrt(variance / GCRs.size());
-
-        sort(table);
-
-        System.out.println("\nGCR-STDEV: " + stddev + " , GCR-MEAN: " + mean + " , NUM_CLUSTERS: " + n + " , MEAN_CLUSTER_SIZE: " + meanClusterSize + " , GCR-PERCENT_OVER_0.5: " + (nOverHalf / n) + " , NUM_UNIQUE_PROTEINS: " + G.G.keySet().size());
+        sort(table, G);
 
         System.out.println("\nTransition proteins: " + obj.transitionNodes);
 
         System.out.println("\nAll proteins: " + G.G.keySet());
+
+        System.out.println("Number of unique proteins: " + G.G.keySet().size() + ", GPI: " + GPI.calculate(results, G, 7) * 100 + "%");
 
         System.out.println("\nApplication took: " + (System.nanoTime()-AllF)/1_000_000.0d + " ms");
         System.out.println("\nClustering took: " + TimeF/1_000_000.0d + " ms");
@@ -71,22 +54,27 @@ public class SingleGO {
      * Sort a hashmap ascendingly, then print output
      * @param in Input HashMap
      */
-    public static void sort(HashMap<Double, Set<String>> in) {
+    public static void sort(HashMap<ArrayList<Double>, Set<String>> in, Graph G) {
         // Create a list of the HashMap's entries.
-        List<Map.Entry<Double, Set<String>>> entries = new ArrayList<>(in.entrySet());
+        List<Map.Entry<ArrayList<Double>, Set<String>>> entries = new ArrayList<>(in.entrySet());
 
         // Sort the list of entries using a custom comparator that compares the Double keys of the entries.
-        Collections.sort(entries, new Comparator<Map.Entry<Double, Set<String>>>() {
+        Collections.sort(entries, new Comparator<Map.Entry<ArrayList<Double>, Set<String>>>() {
             @Override
-            public int compare(Map.Entry<Double, Set<String>> o1, Map.Entry<Double, Set<String>> o2) {
-                return o1.getKey().compareTo(o2.getKey());
+            public int compare(Map.Entry<ArrayList<Double>, Set<String>> o1, Map.Entry<ArrayList<Double>, Set<String>> o2) {
+                /* o1.getKey.get(0) is GCR */
+                /* o1.getKey.get(1) is RC */
+                /* Last term is ratio of nodes in cluster to the whole cluster, 10% means that cluster has 10% of all nodes in network */
+                /* 2.5 coefficient means that for (f/s)V value to increase, GCR needs to be 0.1 higher or RC to be 0.25 higher */
+                double fV = 2.5 * o1.getKey().get(0) + o1.getKey().get(1) + (o1.getValue().size() / G.G.keySet().size());
+                double sV = 2.5 * o2.getKey().get(0) + o2.getKey().get(1) + (o2.getValue().size() / G.G.keySet().size());
+
+                if(fV == sV) return 0;
+                if(fV < sV) return -1;
+                return 1;
             }
         });
 
-        for (Map.Entry<Double, Set<String>> entry : entries) {
-            /* Use the commented line to get GCR values alongside each cluster */
-            //System.out.println(entry.getValue() + " GCR: " + entry.getKey());
-            System.out.println(entry.getValue());
-        }
+        for (Map.Entry<ArrayList<Double>, Set<String>> entry : entries) System.out.println(entry.getValue());
     }
 }
